3af39f2200ba0a04b568d741a48a86e2f154ae86
tikhomirovd <dimasta00@gmail.com>
2018-03-11 00:13:13 +0300

    Libraries was changed

--- library.py
+++ library.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from IPython.display import display
 import mglearn
-plt.rc('font', family='Verdana')
 from sklearn.datasets import load_iris
+from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
+from sklearn.datasets import load_breast_cancer
+plt.rc('font', family='Verdana')
+import mglearn
+
