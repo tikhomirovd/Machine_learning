c8484f244acb0260ddf0e1783f6ad5170faa3c59
tikhomirovd <dimasta00@gmail.com>
2018-03-11 22:06:11 +0300

    LinearSVC and LogisticRegression learn was added

---
+++ linear/Logistic_forge.py
@@ -0,0 +1,17 @@
+from library import *
+
+X, y = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5,
+                                    ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel("Признак 0")
+    ax.set_ylabel("Признак 1")
+axes[0].legend()
+
+plt.show()
