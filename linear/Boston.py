136fc9454d0c295fc3d90b19256995ba107cf762
tikhomirovd <dimasta00@gmail.com>
2018-03-11 20:38:20 +0300

    Lasso learning was added

--- linear/Boston.py
+++ linear/Boston.py
@@ -32,6 +32,37 @@ print("Правильность на обучающем наборе: {:.2f}".fo
 print("Правильность на тестовом наборе: {:.2f}".format(
     ridge01.score(X_test, y_test)))
 
+lasso = Lasso().fit(X_train, y_train)
+print("\nLasso, alpha=1")
+print("Правильность на обучающем наборе: {:.2f}".format(
+    lasso.score(X_train, y_train)))
+print("Правильность на тестовом наборе: {:.2f}".format(
+    lasso.score(X_test, y_test)))
+print("Количество использованных признаков: {}".format(
+    np.sum(lasso.coef_ != 0)))
+
+
+lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print("\nLasso, alpha=0.01, max_iter=100000")
+print("Правильность на обучающем наборе: {:.2f}".format(
+    lasso001.score(X_train, y_train)))
+print("Правильность на тестовом наборе: {:.2f}".format(
+    lasso001.score(X_test, y_test)))
+print("Количество использованных признаков: {}".format(
+    np.sum(lasso001.coef_ != 0)))
+
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("\nLasso, alpha=0.0001, max_iter=100000")
+print("Правильность на обучающем наборе: {:.2f}".format(
+    lasso00001.score(X_train, y_train)))
+print("Правильность на тестовом наборе: {:.2f}".format(
+    lasso00001.score(X_test, y_test)))
+print("Количество использованных признаков: {}".format(
+    np.sum(lasso00001.coef_ != 0)))
+
+
+
+
 plt.plot(ridge.coef_, 's', label="Гребневая регрессия, alpha=1")
 plt.plot(ridge10.coef_, '^', label="Гребневая регрессия, alpha=10")
 plt.plot(ridge01.coef_, 'v', label="Гребневая регрессия, alpha=0.1")
@@ -45,5 +76,17 @@ plt.legend()
 
 plt.subplots()
 mglearn.plots.plot_ridge_n_samples()
+
+plt.subplots()
+
+plt.plot(lasso.coef_, 's', label='Лассо alpha=1')
+plt.plot(lasso001.coef_, '^', label='Лассо alpha=0.01')
+plt.plot(lasso00001.coef_, 'v', label='Лассо alpha=0.0001')
+plt.plot(ridge01.coef_, 'o', label='Гребневая регрессия alpha=0.1')
+
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("Индекс коэффициента")
+plt.ylabel("Оценка коэффициента")
 
 plt.show()
